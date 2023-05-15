9eed109dff2572db905942414d9ab35ee442a7c2
SSully284 <124535993+SSully284@users.noreply.github.com>
2023-05-14 23:14:28 -0400

    Display cities changed and JS updated to show current date and time

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
-//alert("Let's get cracking!");
+function updateTime() {
+  //Georgia
+  let chicagoElement = document.querySelector("#chicago");
+  let chicagoDateElement = chicagoElement.querySelector(".date");
+  let chicagoTimeElement = chicagoElement.querySelector(".time");
+  let chicagoTime = moment().tz("America/Chicago");
+
+  chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
+  chicagoTimeElement.innerHTML = chicagoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Yukon
+  let yukonElement = document.querySelector("#yukon");
+  let yukonDateElement = yukonElement.querySelector(".date");
+  let yukonTimeElement = yukonElement.querySelector(".time");
+  let yukonTime = moment().tz("Canada/Yukon");
+
+  yukonDateElement.innerHTML = yukonTime.format("MMMM Do YYYY");
+  yukonTimeElement.innerHTML = yukonTime.format("h:mm:ss [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
